54063618f2adbc3bf319a20b4ea522cf6a7620f9
Luckas <luckasranarison@gmail.com>
2023-03-08 18:06:28 +0300

    update: add icons

--- src/components/DetailsPopup.jsx
+++ src/components/DetailsPopup.jsx
@@ -1,5 +1,10 @@
 import React, { useContext } from "react";
-import { XCircleIcon } from "@heroicons/react/20/solid";
+import {
+  XCircleIcon,
+  ClockIcon,
+  BookOpenIcon,
+  InformationCircleIcon as InfoIcon,
+} from "@heroicons/react/20/solid";
 import { Context } from "./ContextProvider";
 
 const DetailsPopoup = ({ task }) => {
@@ -7,15 +12,24 @@ const DetailsPopoup = ({ task }) => {
 
   return (
     <div className="z-20 absolute inset-0 w-screen h-screen flex items-center justify-center bg-dark opacity-95">
-      <div className="py-2 px-4 max-w-[360px] w-5/6 h-56 relative text-dark bg-light overflow-y-scroll">
+      <div className="p-4 max-w-[360px] w-5/6 h-56 relative text-dark bg-light overflow-y-scroll">
         <button
           className="w-8 absolute top-1 right-1 text-red"
           onClick={() => setOpenPopupId()}
           children={<XCircleIcon />}
         ></button>
-        <p>Added on {task.date}</p>
-        <p>Description: {task.content}</p>
-        <p>Status: {task.completed ? "Done" : "Unfinished"}</p>
+        <p className="flex">
+          <ClockIcon className="mr-2 w-5" />
+          Added on {task.date}
+        </p>
+        <p className="flex">
+          <BookOpenIcon className="mr-2 w-5" />
+          Description: {task.content}
+        </p>
+        <p className="flex">
+          <InfoIcon className="mr-2 w-5" />
+          Status: {task.completed ? "Done" : "Unfinished"}
+        </p>
       </div>
     </div>
   );
